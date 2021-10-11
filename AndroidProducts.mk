64349973df875de82f6522284e85624e0ed0b1c9
Kay Shinonome <kayshinonome@protonmail.com>
2021-10-10 19:40:31 -0500

    adding generated configs

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_U304AA.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_U304AA-user \
+    omni_U304AA-userdebug \
+    omni_U304AA-eng
---
+++ omni_U304AA.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from U304AA device
+$(call inherit-product, device/tinno/U304AA/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := U304AA
+PRODUCT_NAME := omni_U304AA
+PRODUCT_BRAND := ATT
+PRODUCT_MODEL := U304AA
+PRODUCT_MANUFACTURER := tinno
